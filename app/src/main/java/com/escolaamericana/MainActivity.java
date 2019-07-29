0e69e362af4f468bb7d19d1138453791aee22b82
thiag <thiago.berwanger@gmail.com>
2019-07-29 14:59:37 -0300

    Versão Teste

--- app/src/main/java/com/escolaamericana/MainActivity.java
+++ app/src/main/java/com/escolaamericana/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private Button Login;
     private RequestQueue mQueue;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
