f65bd6119c0998ad59c9fa0b7b2328b92b76450d
thiag <thiago.berwanger@gmail.com>
2019-10-29 17:19:16 -0300

    Versão quase pronta 2.0

--- app/src/main/java/com/escolaamericana/FourthActivity.java
+++ app/src/main/java/com/escolaamericana/FourthActivity.java
@@ -1,12 +1,25 @@
 package com.escolaamericana;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -37,6 +50,8 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.zxing.integration.android.IntentIntegrator;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -45,9 +60,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+
 public class FourthActivity extends AppCompatActivity {
     ListView listView;
     Button saidaAluno;
+    String pid;
+    private ProgressDialog pDialog;
+    private static final String TAG_SUCCESS = "success";
+    // JSON parser class
+    JSONParser jsonParser = new JSONParser();
 
     final String alunosArray [] = new String[50];
     private RequestQueue mQueue;
@@ -96,6 +117,7 @@ public class FourthActivity extends AppCompatActivity {
                         switch (which) {
                             case DialogInterface.BUTTON_POSITIVE:
                                 // User clicked the Yes button
+                                setPos(pos);
                                 for (int i = pos; i < alunosArray.length; i++){
                                     if (i == alunosArray.length - 1){
                                         alunosArray[i] = "";
@@ -162,45 +184,44 @@ public class FourthActivity extends AppCompatActivity {
         final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, alunosArray);
         listView.setAdapter(arrayAdapter);
     }
-    private void validate (final String userName, final String userPassword){
-        String url = "http://restrito.ccbeu.com/android_connect/get_all_alunos.php";
-        final JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    JSONArray jsonArray = response.getJSONArray("dados_alunos");
-                    boolean sucesso = false;
-                    for (int i = 0; i < jsonArray.length(); i++){
-                        JSONObject pais = jsonArray.getJSONObject(i);
 
-                        String login = pais.getString("usuario");
-                        String senha = pais.getString("senha");
-                        String filho = pais.getString("nome_filho");
-                        Usuario usuario = new Usuario();
-                        usuario.setFilho(filho);
-                        Session session = new Session(usuario);
-                        alunosArray[i] = filho;
+    private void setPos (final int teste2){
+
+        String url = "http://restrito.ccbeu.com/android_connect/get_all_alunos_temp.php";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        int test = teste2;
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("alunos_temp");
+
+                            JSONObject position = jsonArray.getJSONObject(test);
+                            String positionString = position.getString("pid");
+                            pid = positionString;
+                            new DeleteProduct().execute();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
+                }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                setPos(teste2);
                 error.printStackTrace();
             }
         });
         mQueue.add(request);
     }
 
+
     private void getAluno() {
         String url = "http://restrito.ccbeu.com/android_connect/get_all_alunos_temp.php";
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>() {
 
-                    @Override
+
                     public void onResponse(JSONObject response) {
                         try {
                             JSONArray jsonArray = response.getJSONArray("alunos_temp");
@@ -225,4 +246,76 @@ public class FourthActivity extends AppCompatActivity {
         mQueue.add(request);
     }
 
+//    private void deleteAluno () {
+//        String url_delete_product = "http://restrito.ccbeu.com/android_connect/delete_aluno_saida.php";
+//
+//
+//        // Check for success tag
+//        int success;
+//        try {
+//            // Building Parameters
+//            List<NameValuePair> params = new ArrayList<NameValuePair>();
+//            params.add(new BasicNameValuePair("pid", pid));
+//
+//            // getting product details by making HTTP request
+//            JSONObject json = jsonParser.makeHttpRequest(
+//                    url_delete_product, "POST", params);
+//
+//            // check your log for json response
+//
+//            // json success tag
+//            success = json.getInt(TAG_SUCCESS);
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+    class DeleteProduct extends AsyncTask<String, String, String> {
+        String url_delete_product = "http://restrito.ccbeu.com/android_connect/delete_aluno_saida.php";
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(FourthActivity.this);
+            pDialog.setMessage("Deleting Product...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        /**
+         * Deleting product
+         * */
+        protected String doInBackground(String... args) {
+
+            // Check for success tag
+            int success;
+                // Building Parameters
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("pid", pid));
+
+                // getting product details by making HTTP request
+                JSONObject json = jsonParser.makeHttpRequest(
+                        url_delete_product, "POST", params);
+
+                // check your log for json response
+//                Log.d("Delete Product", json.toString());
+
+                // json success tag
+
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once product deleted
+            pDialog.dismiss();
+
+        }
+
+    }
 }
